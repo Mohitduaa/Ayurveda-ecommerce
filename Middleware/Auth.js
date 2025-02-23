11a01cb84c1cfae0b331cd300ca043984e35f0d0
Mohit Dua <duamohit10@gmail.com>
2025-02-23 17:25:16 +0530

    auth changes

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -1,31 +1,38 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-const Auth = async(req,res,next) =>{
-    try{
-        const token = req.cookies.accesstoken ||req?.header?.authorization.split(" ")[1]
-        if(!token){
+const Auth = async (req, res, next) => {
+    try {
+        const token = req.cookies.accesstoken || (req.headers.authorization ? req.headers.authorization.split(" ")[1] : null);
+        
+        if (!token) {
             return res.status(401).json({
-                message:"Provide token"
-            })
-        } 
-        const decode = await jwt.verify(token,process.env.SECRET_KEY_ACCESS_TOKEN)  
-        if(!decode){
-            return res.status(401).json({
-                message : "Unauthorized acess",
-                error:true,
-                success:false
-            })
+                message: "Provide token",
+                error: true,
+                success: false
+            });
         }
-        req.userId = decode.id
-        next()
-        console.log("decode",decode);
-             
-    }catch(error){
+
+        const decode = await jwt.verify(token, process.env.SECRET_KEY_ACCESS_TOKEN);
+        
+        if (!decode) {
+            return res.status(401).json({
+                message: "Unauthorized access",
+                error: true,
+                success: false
+            });
+        }
+
+        req.userId = decode.id;
+        next();
+        console.log("Decoded Token:", decode);
+
+    } catch (error) {
         return res.status(500).json({
-            message:error.message|| error,
-            error :true,
-            success :false
-        })
+            message: error.message || error,
+            error: true,
+            success: false
+        });
     }
-}
-export default Auth
+};
+
+export default Auth;
